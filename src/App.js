cb732b3fca5f534b164b22477b8184fc33460539
kxngJ <jaheimcbrown@gmail.com>
2021-12-06 08:12:53 -0500

    reviews react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import data from "./data";
 
 function App() {
+  // My state
+  const [state, setState] = useState(0);
+  const [current, setCurrent] = useState({});
+
+  const filterUsers = (id) => {
+    const currentUser = data.filter((user) => user.id === id);
+    return currentUser[0];
+  };
+
+  // update state
+  const incState = () => {
+    if (state === 3) {
+      setState(0);
+      return;
+    }
+    setState(state + 1);
+  };
+
+  const decState = () => {
+    if (state === 0) {
+      setState(3);
+      return;
+    }
+    setState(state - 1);
+  };
+
+  const randomState = () => {
+    let randomUser = Math.floor(Math.random() * 4);
+    console.log(randomUser);
+    setState(randomUser);
+  };
+
+  // useEffect hook
+  useEffect(() => {
+    let user = filterUsers(state);
+    setCurrent(user);
+  }, [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {current ? (
+        <>
+          <h1>Our Reviews</h1>
+          <Template
+            current={current}
+            increment={incState}
+            decrement={decState}
+            random={randomState}
+          />
+        </>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </>
   );
 }
+
+const Template = ({ current, increment, decrement, random }) => {
+  const { image, name, role, review } = current;
+  return (
+    <>
+      <div className='container'>
+        <img src={image} alt={name}></img>
+        <h4 className='author'>{name}</h4>
+        <h5 className='job'>{role}</h5>
+        <article>
+          <p className='review'>{review}</p>
+        </article>
+        <div className='btnContainer'>
+          <button className='back' onClick={decrement}>
+            <svg
+              stroke='currentColor'
+              fill='currentColor'
+              stroke-width='0'
+              viewBox='0 0 320 512'
+              height='1em'
+              width='1em'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z'></path>
+            </svg>
+          </button>
+          <button className=' forward' onClick={increment}>
+            <svg
+              style={{ transform: "rotate(180deg)" }}
+              stroke='currentColor'
+              fill='currentColor'
+              stroke-width='0'
+              viewBox='0 0 320 512'
+              height='1em'
+              width='1em'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z'></path>
+            </svg>
+          </button>
+        </div>
+        <button className='random' onClick={random}>
+          Surprise Me
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    id: 0,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "Peter Jones",
+    role: "INTERN",
+    review:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    name: "Bill Anderson",
+    role: "THE BOSS",
+    review:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Susan Smith",
+    role: "WEB DEVELOPER",
+    review:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry.",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    name: "Anna Johnson",
+    role: "WEB DESIGNER",
+    review:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+];
+
+export default data;
